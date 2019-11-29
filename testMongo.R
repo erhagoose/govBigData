a148a75d58f48d361de2769e595f807e1279e7a3
haotian <ritou11@gmail.com>
2019-11-29 19:18:44 +0800

    learn R

--- testMongo.R
+++ testMongo.R
@@ -1,12 +1,30 @@
 library("mongolite")
-mposts <- mongo("posts", url = "mongodb://localhost:27017/wechat_spider")
-mpubs <- mongo("profiles", url = "mongodb://localhost:27017/wechat_spider")
-print(mpubs)
+mongourl <- "mongodb://localhost:27017/wechat_spider"
+mposts <- mongo("posts", url = mongourl)
+mpubs <- mongo("profiles", url = mongourl)
+
 mposts$count('{}')
 mpubs$count('{}')
 posts <- mposts$find(
   query = '{}', 
-  fields = '{"_id": false, "msgBiz" : true, "msgMid" : true, "msgIdx" : true, "title" : true}',
-  limit = 10
+  fields = '{"_id": false,
+            "msgBiz" : true,
+            "msgMid" : true,
+            "msgIdx" : true,
+            "title" : true,
+            "publishAt" :true,
+            "readNum": true,
+            "likeNum": true
+          }',
+  sort = '{ "publishAt": -1 }'
 )
-print(posts)
+# summary(posts)
+nposts = na.omit(posts)
+summary(nposts)
+boxplot(nposts$readNum, main="All Posts: readNum", ylab="Times")
+boxplot(nposts$likeNum, main="All Posts: likeNum", ylab="Times")
+hist(nposts$readNum, main="All Posts: readNum", xlab="Times")
+hist(nposts$likeNum, main="All Posts: likeNum", xlab="Times")
+
+library("ggplot2")
+ggplot(data=nposts, aes(x=readNum, y=likeNum)) + geom_point()
