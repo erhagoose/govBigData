e2e36e6eddee90aa4cb3850e0b4cad46bcc4399f
ritou11 <ritou11@gmail.com>
2019-12-02 22:59:43 +0800

    add type to profiles

---
+++ handleProfiles.R
@@ -0,0 +1,31 @@
+library("mongolite")
+library("ggplot2")
+library("ggpubr")
+
+# mongodb
+mongourl <- "mongodb://192.10.10.108:27017/wechat_spider"
+mpubs <- mongo("profiles", url = mongourl)
+mpubs$count('{}')
+
+profiles <- mpubs$find(
+  query = '{}', 
+  fields = '{"_id": false,
+            "msgBiz" : true,
+            "title" : true,
+            "openHistoryPageAt" :true,
+            "updatedAt": true,
+            "type": true
+          }'
+)
+
+write.csv(profiles, 'outputs/profiles.csv')
+
+ep <- read.csv('outputs/profiles.csv', stringsAsFactors = FALSE)
+attach(ep)
+for (i in 1:nrow(ep)) {
+  mpubs$update(
+    query = sprintf('{ "msgBiz": "%s" }', msgBiz[i]),
+    update = sprintf('{"$set":{ "type": "%s" }}', type[i])
+  )
+}
+detach(ep)
