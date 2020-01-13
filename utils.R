3b6095c2338debb50d8cee32a453250c76f0f0cb
haotian <ritou11@gmail.com>
2020-01-13 17:38:43 +0800

    WIP: report, xiaoli

--- utils.R
+++ utils.R
@@ -1,7 +1,7 @@
 ## Post Propagation Index
 ppi <- function (readNum, likeNum) {
   if (readNum > 100000) { readNum = readNum * 1.5 }
-  if (likeNum > 100000) { likeNum = likeNum * 1.5 }
+  if (likeNum > 100000) { likeNum = likeNum * 1.2 }
   return(0.85 * log(1 + readNum)  + 0.15 * log(1 + likeNum))
 }
 
